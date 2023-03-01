84e1493a539aa9b28017e77bcd8cb253dd76f143
renechoi <renechoi90@gmail.com>
2023-03-01 18:22:27 +0900

    Initial commit

---
+++ src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -0,0 +1,14 @@
+package hello.thymeleafbasic.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    public String textBasic(Model model){
+
+    }
+}
