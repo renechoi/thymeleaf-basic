5042a822aae8a863b9e29f21ac962a1e5bdf21a8
renechoi <renechoi90@gmail.com>
2023-03-02 11:08:02 +0900

    feat: thyme leaf - iteration

--- src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -92,6 +93,21 @@ public class BasicController {
     @GetMapping("attribute")
     public String attribute(){
         return "basic/attribute";
+    }
+
+    @GetMapping("each")
+    public String each(Model model){
+        addUsers(model);
+        return "basic/each";
+    }
+
+    public void addUsers(Model model){
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+
+        model.addAttribute("users", list);
     }
 
     @Data
