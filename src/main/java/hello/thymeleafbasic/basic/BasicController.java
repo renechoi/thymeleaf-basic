7dfe94337f66a03d7ee79f6127f113350d15a225
renechoi <renechoi90@gmail.com>
2023-03-02 15:47:51 +0900

    feat: thyme leaf -js, block

--- src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -121,6 +121,22 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model){
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+        model.addAttribute("user", new User("userA", 10) );
+        addUsers(model);
+
+        return "basic/javascript";
+    }
+
+
+
     @Data
     static class User {
         private String username;
